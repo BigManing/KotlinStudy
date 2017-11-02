4ce398ad14353258bf68b586658c747e493fd189
BigManing <lingshui2008@qq.com>
2017-11-02 14:29:25 +0800

    数据类-copy

--- app/src/test/java/com/example/jiang/myapplication/class/Data.kt
+++ app/src/test/java/com/example/jiang/myapplication/class/Data.kt
@@ -7,22 +7,28 @@ import org.junit.Test
  * 数据类 == java中的 javabean
  *
  */
-class Data {
-    @Test
-    fun t() {
-        val user = User("张三", 10,"ok")
-        user.age=10
-        user.copy()
-        println(user.component1())
-    }
-}
-
 ///////////////////////////////////////////////////////////////////////////
 // 1 数据类的构造函数至少有一个参数
 // 2 参数修饰： val 或者 var
 // 3 不能抽象 开放 封闭
 // 4 实现接口 或者 密封类
 ///////////////////////////////////////////////////////////////////////////
-data class  User(val name: String, var age: Int,val s: String)
+data class User(val name: String, var age: Int, val s: String)
+
 // 此时会生成无参的构造函数
-data class  User1(val name: String="", var age: Int=1,val s: String="")  //
+data class User1(val name: String = "", var age: Int = 1, val s: String = "")  //
+
+
+class Data {
+    @Test
+    fun t() {
+        val user = User("张三", 10, "ok")
+        user.age = 10
+///////////////////////////////////////////////////////////////////////////
+// 复制  把需要变更的写在参数里
+///////////////////////////////////////////////////////////////////////////
+        val copy = user.copy(name = "李四")
+        println(copy.toString())
+    }
+}
+
