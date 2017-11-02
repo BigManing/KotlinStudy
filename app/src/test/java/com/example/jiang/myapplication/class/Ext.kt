14c9fe156655e7d04e008ac21fb29136d9c354b2
BigManing <lingshui2008@qq.com>
2017-11-02 14:01:32 +0800

    扩展函数-定义在别的类中

--- app/src/test/java/com/example/jiang/myapplication/class/Ext.kt
+++ app/src/test/java/com/example/jiang/myapplication/class/Ext.kt
@@ -80,7 +80,7 @@ fun Any?.toString(): String {
 // 由于扩展没有实际的将成员插入类中，因此对扩展属性来说幕后字段是无效的。
 // 这就是为什么扩展属性不能有初始化器。他们的行为只能由显式提供的 getters/setters 定义。
 ///////////////////////////////////////////////////////////////////////////
-val<T> List<T>.lastIndexMy: Int
+val <T> List<T>.lastIndexMy: Int
     get() = size - 1
 
 //val<T> List<T>.lastIndexMy1: Int=2   这个就是错误的
@@ -88,4 +88,17 @@ val<T> List<T>.lastIndexMy: Int
 
 fun f() {
     println(listOf(1, 2).lastIndexMy)
+}
+
+///////////////////////////////////////////////////////////////////////////
+// 可以在A类中定义其他类的扩展（扩展声明为成员）
+///////////////////////////////////////////////////////////////////////////
+
+class F {
+    fun D.to() {
+//       d类的方法
+        toString()
+//       f类的方法
+        this@F.toString()
+    }
 }
