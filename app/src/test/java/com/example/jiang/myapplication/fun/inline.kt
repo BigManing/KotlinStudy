690b7dd6b5c8c0585d106d94fdcc2ab8243265c7
BigManing <lingshui2008@qq.com>
2017-11-07 11:01:59 +0800

    inline-reified  具体化类型参数  --- 具体写法（方法必须是inline修饰）

--- app/src/test/java/com/example/jiang/myapplication/fun/inline.kt
+++ app/src/test/java/com/example/jiang/myapplication/fun/inline.kt
@@ -55,4 +55,19 @@ class Inline {
 //    }
 //    TreeNode.findParentOfType(MyTreeNode::class.java)
 
+    /*2 具体化的类型参数 （方法必须是inline修饰的） 的写法以及调用
+
+    inline fun <reified T> TreeNode.findParentOfType(): T? {
+        var p = parent
+        while (p != null && p !is T) {
+            p = p.parent
+        }
+        return p as T?
+    }
+
+我们使用 reified 修饰符来限定类型参数，现在可以在函数内部访问它了， 几乎就像是一个普通的类一样。
+ 由于函数是内联的，不需要反射，正常的操作符如 !is 和 as 现在都能用了。
+ 此外，我们还可以按照上面提到的方式调用它：
+ myTree.findParentOfType<MyTreeNodeType>()。
+ */
 }
