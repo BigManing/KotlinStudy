60d144ed9e47346d6628b00c4ce93928bc7b24be
BigManing <lingshui2008@qq.com>
2017-11-13 13:29:44 +0800

    java代码中引用Kotlin--类 方法的调用

--- app/src/test/java/com/example/jiang/myapplication/withJava/MyClass.java
+++ app/src/test/java/com/example/jiang/myapplication/withJava/MyClass.java
@@ -2,11 +2,15 @@ package com.example.jiang.myapplication.withJava;
 
 /**
  * Created by BigManing on 17-11-13.
+ * 引用kotlin中的东西
  */
 
 public class MyClass {
     public void remove(int... x) {
         System.out.println(x.length);
-
+        //1 kotlin中的JavaInKotlin类
+        new JavaInKotlin().aa();
+        //2 kotlin中包级别的函数：文件名+Kt+fun名称
+        JavaInKotlinKt.kebiancanshu();
     }
 }
